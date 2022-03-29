71b6c72616a778b13a0fe40a0d84ed68c1ea6800
折木凛曦 <78236470+orikiringi@users.noreply.github.com>
2022-03-30 04:00:09 +0900

    Update 原神预测体力工具.py

--- 原神预测体力工具.py
+++ 原神预测体力工具.py
@@ -5,8 +5,8 @@ import calendar
 from platform import system
 
 osname=system()
-if osname == 'Windows':os.system("mode con cols=79 lines=20")
-elif osname == "Linux" or osname == "Darwin":os.system("printf '\033[8;20;79t'")
+if osname == 'Windows':os.system("mode con cols=79 lines=20");clsswitch="cls"
+elif osname == "Linux" or osname == "Darwin":os.system("printf '\033[8;20;79t'");clsswitch="clear"
 
 def Direct_cal():
     times=time.time()
@@ -29,7 +29,7 @@ def Direct_cal():
             FUL_Physical_strength=input("需预测体力(请只输入数字)：") 
              
     TIME=(int(FUL_Physical_strength)-int(NOW_Physical_strength))*480
-    print("预测时间：\n",time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(times+TIME)),"~",time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(times+TIME+480)),"\n")
+    print("\n预测时间：\n",time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(times+TIME)),"~",time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(times+TIME+480)),"\n")
 
 def Indirect_cal():
     NOW_time=time.time()
@@ -64,17 +64,18 @@ def Indirect_cal():
     Now_Physical_strength=input("当前体力(若需要预测时间超过21小时20分，请输入0):\n>> ").replace("-"," ")
     while not re.findall('^[0-9]+$',Now_Physical_strength):
         Now_Physical_strength=input("当前体力(请只输入数字)：\n>> ").replace("-"," ")
-        
-    CHK_TIME=NOW_time+(160-int(Now_Physical_strength))*480-time.mktime(time.strptime(Target_date,"%Y %m %d %H %M %S"))
     
-    if CHK_TIME > 0:
-        print("按照预计您的体力值在预定时间将会超过最大体力值 \033[93m%s\033[0m 点请用掉这些体力\n然后从此时间开始计算到指定日期时间将会是满格体力。\n" % int(CHK_TIME/480))
-    elif CHK_TIME + 160*480 > 0:
-        TIMES=time.mktime(time.strptime(Target_date,"%Y %m %d %H %M %S"))-NOW_time-(160-int(Now_Physical_strength))*480
-        print("按照预计您的体力值在预定时间将会超过最大体力值 \033[93m%s\033[0m 点请用掉这些体力\n然后从此时间开始计算到指定日期时间将会是满格体力。\n" % int(TIMES/480))
+    TIMES=time.mktime(time.strptime(Target_date,"%Y %m %d %H %M %S"))-NOW_time-(160-int(Now_Physical_strength))*480
+    
+    print("")
+    
+    if TIMES > 0 and TIMES < 76800:
+        print("按照预计您的体力值在%s满格\n在预定时间将会超过最大体力值 \033[93m%s\033[0m 点，请用掉这些体力\n然后从此时间开始计算到指定日期时间将会是满格体力。\n" % (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(NOW_time+(160-int(Now_Physical_strength))*480)),int(TIMES/480)))
     else:
         TIME=time.mktime(time.strptime(Target_date,"%Y %m %d %H %M %S"))-160*480
-        print("体力满格前推计算时间为：\n",time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(TIME)),"\n")
+        print("体力满格前推计算时间为：\033[93m\n",time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(TIME)),"\033[0m")
+        if TIMES < 76800:
+            print("至目标时间时保有体力值：\033[93m\n",int((time.mktime(time.strptime(Target_date,"%Y %m %d %H %M %S"))-NOW_time)/480+int(Now_Physical_strength)),"\033[0m 点体力。")
 
 while True:
     sec=input("1.预测体力满格时间\n2.体力满格前推计算时间\n>>")
@@ -88,9 +89,10 @@ while True:
     else:
         print("输入错误请重新输入。")
     
-    secny=input("继续(Y),退出(N) >>")
+    secny=input("\n继续(Y),退出(N) >>")
     while not re.findall('^[NYny]+$',secny):
         secny=input("请输入正确的选项\n继续(Y),退出(N) >>")
     if secny in {"N","n"}:
         os._exit(0)
     print("\n")
+    os.system(clsswitch)
