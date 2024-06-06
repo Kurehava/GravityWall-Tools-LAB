b0bc57ff477b44e05a7c88e94a881b2ef218797b
折木凛曦 <78236470+Kurehava@users.noreply.github.com>
2024-06-06 11:09:48 +0900

    fix bug

--- 1.SystemScripts/WindowsScripts/PowerShellProfile/newProfile.ps1
+++ 1.SystemScripts/WindowsScripts/PowerShellProfile/newProfile.ps1
@@ -1,4 +1,5 @@
 $history = New-Object System.Collections.ArrayList
+$MAX_HISTORY = 500
 
 function prompt {
     $isRoot = (([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
@@ -31,6 +32,9 @@ function newcd(){
   [void] $history.Add((Get-Location).Path)
   Set-Location $Args[0]
   newls
+  if ($history.Count -gt $MAX_HISTORY){
+    $history.RemoveAt(0)
+  }
 }
 
 function back(){
@@ -38,11 +42,11 @@ function back(){
     Write-Output "can not back."
   }else{
     Set-Location $history[-1]
-    $history.RemoveAt(($history.Count) - 1)
     if ($Args[0] -eq "-l" -or $args[0] -eq "--list"){
       newls
     }
-    if ($history.Count -gt 500){
+    $history.RemoveAt(($history.Count) - 1)
+    if ($history.Count -gt $MAX_HISTORY){
       $history.RemoveAt(0)
     }
   }
@@ -56,8 +60,11 @@ function historys(){
       $elemcount += 1
     }
   }else{
-    if ([int]::TryParse($Args[0], [ref]$null) -eq "True"){
+    if ([int]::TryParse($Args[0], [ref]$null) -eq "True" -and [int]$Args[0] -lt $history.Count){
       Set-Location $history[$Args[0]]
+      foreach ($r in $history.Count..([int]$Args[0] + 1)){
+        $history.RemoveAt($r - 1)
+      }
     }else{
       Write-Output "You must input a number."
     }
