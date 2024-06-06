c54f0933b267c5d1963d48c2617559fd3ed1e4dc
折木凛曦 <78236470+Kurehava@users.noreply.github.com>
2024-06-06 11:11:37 +0900

    Create newls.ps1

---
+++ 1.SystemScripts/WindowsScripts/PowerShellProfile/ProfileFunctions/newls.ps1
@@ -0,0 +1,7 @@
+function newls(){
+  if($Args[0] -eq "-all" -or $Args[0] -eq "-a"){
+    Get-ChildItem | Select-Object Mode,LastWriteTime,Length,Name | ft -Wrap
+  }else{
+    Get-ChildItem | Select-Object Mode,LastWriteTime,Length,Name
+  }
+}
