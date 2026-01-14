ac6f2e71418ad04a2db6f794da8845e531807d12
折木凛曦 <78236470+Kurehava@users.noreply.github.com>
2026-01-14 13:50:10 +0900

    Update ZSH theme and plugins installation script

--- 1.SystemScripts/LinuxScripts/ZSH/ZSH_INSTALL_AUTO.sh
+++ 1.SystemScripts/LinuxScripts/ZSH/ZSH_INSTALL_AUTO.sh
@@ -199,10 +199,10 @@ omz_themes="$omz_home/themes"
 omz_plugins="$omz_home/plugins"
 
 # get kurehava zsh theme
-`$download_command "${boots_proxy}https://raw.githubusercontent.com/Kurehava/GravityWall-Tools-LAB/main/1.SystemScripts/LinuxScripts/ZSH/KENSYO/kensyo_theme.zsh-theme" > "$omz_themes/kensyo.zsh-theme"`
+`$download_command "${boots_proxy}https://raw.githubusercontent.com/Kurehava/GravityWall-Tools-LAB/refs/heads/main/1.SystemScripts/LinuxScripts/ZSH/sysinfo.zsh-theme" > "$omz_themes/sysinfo.zsh-theme"`
 
 # change theme to kurehava conda theme
-$(sed -i s:robbyrussell::g "$user_root/.zshrc")
+$(sed -i s:robbyrussell:sysinfo:g "$user_root/.zshrc")
 
 # install plugins
 $(sed -i 's:plugins=(git):plugins=(git z extract):g' "$user_root/.zshrc")
