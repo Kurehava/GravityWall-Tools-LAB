7364845a510d904f095fad941990b3b39d0f6fa6
折木凛曦 <78236470+Kurehava@users.noreply.github.com>
2026-04-08 09:17:30 +0900

    Fix alias syntax for Ubuntu and Debian maintenance

--- 1.SystemScripts/LinuxScripts/CommandAlias/maintenance.sh
+++ 1.SystemScripts/LinuxScripts/CommandAlias/maintenance.sh
@@ -5,4 +5,4 @@ alias maintenance="sudo dnf makecache && sudo dnf -y upgrade && sudo dnf -y auto
 alias maintenance="sudo yum makecache && sudo yum -y update && sudo yum -y autoremove"
 
 # ubuntu & debian
-alias maintenance="sudo apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confold" upgrade && sudo DEBIAN_FRONTEND=noninteractive apt-get -y autoremove"
+alias maintenance='sudo apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confold" upgrade && sudo DEBIAN_FRONTEND=noninteractive apt-get -y autoremove'
