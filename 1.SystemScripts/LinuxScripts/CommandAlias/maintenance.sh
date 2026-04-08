eafae9709744987dda39c3ab2df3354361662a64
折木凛曦 <78236470+Kurehava@users.noreply.github.com>
2026-04-08 09:17:13 +0900

    Update maintenance alias for CentOS 8 and 7

--- 1.SystemScripts/LinuxScripts/CommandAlias/maintenance.sh
+++ 1.SystemScripts/LinuxScripts/CommandAlias/maintenance.sh
@@ -1,5 +1,8 @@
-# centos
-alias maintenance="sudo apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confold" upgrade && sudo DEBIAN_FRONTEND=noninteractive apt-get -y autoremove"
+# centos 8+
+alias maintenance="sudo dnf makecache && sudo dnf -y upgrade && sudo dnf -y autoremove"
+
+# CentOS 7
+alias maintenance="sudo yum makecache && sudo yum -y update && sudo yum -y autoremove"
 
 # ubuntu & debian
 alias maintenance="sudo apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confold" upgrade && sudo DEBIAN_FRONTEND=noninteractive apt-get -y autoremove"
