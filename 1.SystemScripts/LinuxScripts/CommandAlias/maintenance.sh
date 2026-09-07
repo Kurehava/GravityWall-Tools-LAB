499481fcea2498c3717df8d09a2b1be4df07f0a3
折木凛曦 <78236470+Kurehava@users.noreply.github.com>
2026-09-08 07:54:30 +0900

    Update maintenance.sh

--- 1.SystemScripts/LinuxScripts/CommandAlias/maintenance.sh
+++ 1.SystemScripts/LinuxScripts/CommandAlias/maintenance.sh
@@ -9,3 +9,7 @@ alias maintenance='sudo apt-get update && sudo DEBIAN_FRONTEND=noninteractive ap
 
 # ubuntu & debian full-upgrade
 alias maintenance_full='sudo apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confold" full-upgrade && sudo DEBIAN_FRONTEND=noninteractive apt-get -y autoremove'
+
+# ubuntu & debian both
+alias maintenance='sudo apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confold" upgrade && sudo DEBIAN_FRONTEND=noninteractive apt-get -y autoremove'
+alias maintenance_full='sudo apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confold" full-upgrade && sudo DEBIAN_FRONTEND=noninteractive apt-get -y autoremove'
