a53f5f44985305db31f6b1231e092f8c9c2490fb
折木凛曦 <78236470+Kurehava@users.noreply.github.com>
2026-09-09 17:31:39 +0900

    Update timeping.ps1

--- 1.SystemScripts/WindowsScripts/PowerShellProfile/ProfileFunctions/timeping.ps1
+++ 1.SystemScripts/WindowsScripts/PowerShellProfile/ProfileFunctions/timeping.ps1
@@ -1,5 +1,15 @@
-function timeping(){
-  C:\WINDOWS\system32\PING.EXE $Args | ForEach-Object { "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - $_" }
-}
+function ping {
+    param(
+        [Parameter(ValueFromRemainingArguments = $true)]
+        [string[]]$PingArgs
+    )
 
-Set-Alias -Name ping -Value timeping -Option AllScope
+    & "$env:SystemRoot\System32\PING.EXE" @PingArgs |
+        ForEach-Object {
+            if ($_ -eq '') {
+                ''
+            } else {
+                "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') $_"
+            }
+        }
+}
