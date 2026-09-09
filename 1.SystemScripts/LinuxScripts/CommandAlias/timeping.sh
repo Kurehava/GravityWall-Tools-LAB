0784c15eac47fbfc33b3eb1f6ece291ee2d10fd7
折木凛曦 <78236470+Kurehava@users.noreply.github.com>
2026-09-09 17:40:41 +0900

    Update timeping.sh

--- 1.SystemScripts/LinuxScripts/CommandAlias/timeping.sh
+++ 1.SystemScripts/LinuxScripts/CommandAlias/timeping.sh
@@ -1,5 +1,10 @@
 ping() {
     command ping -O "$@" | while IFS= read -r line; do
-        printf '%s %s\n' "$(date '+%Y-%m-%d %H:%M:%S')" "$line"
+        printf '%s - %s\n' "$(date '+%Y-%m-%d %H:%M:%S')" "$line"
     done
 }
+
+# if want use default ping command
+# you can use Command command to use original ping.
+# e.g.
+# command ping -O 8.8.8.8
